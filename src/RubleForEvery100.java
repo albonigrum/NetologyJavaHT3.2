e831917d14638c8cb763cf6419280ce6e55c8032
Ivan Grebenschikov <62839684+albonigrum@users.noreply.github.com>
2020-06-09 21:58:28 +0300

    CamelCase naming in RubleForEvery100.java

--- src/RubleForEvery100.java
+++ src/RubleForEvery100.java
@@ -5,12 +5,12 @@ public class RubleForEvery100 {
         if (args.length != 1)
             throw new IllegalArgumentException("Number of arguments not equal 1");
         long balance = 100;
-        long money_transfer = Long.parseLong(args[0]);
+        long moneyTransfer = Long.parseLong(args[0]);
         long bonus = 0;
-        if (money_transfer >= LOW_BOUND_BONUS)
-            bonus += money_transfer / 100;
+        if (moneyTransfer >= LOW_BOUND_BONUS)
+            bonus += moneyTransfer / 100;
 
-        balance += money_transfer + bonus;
+        balance += moneyTransfer + bonus;
 
         System.out.println("Итоговый бонус: " + bonus);
         System.out.println("Итоговый счёт:  " + balance);
